767ec95f49f9f30f11277250599975c3c933d88a
Calkwalk <calkwalk@163.com>
2023-11-17 11:40:22 +0800

    Real EState App Firest Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
+
+export default App
