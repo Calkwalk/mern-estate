87b2da97ced95973dde04b6ef4d221594caf3367
Calkwalk <calkwalk@163.com>
2023-11-17 12:12:15 +0800

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Home, About, SignIn, SignOut, Profile} from './pages';
 function App() {
 
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!
-      </h1>
-    </main>
-  )
+    <Router>
+      <Routes>
+        <Route path='/'         element={ <Home />      } />
+        <Route path='/about'    element={ <About />     } />
+        <Route path='/profile'  element={ <Profile />   } />
+        <Route path='/signin'   element={ <SignIn />    } />
+        <Route path='/signout'  element={ <SignOut />   } />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,13 @@
+import Home     from './Home';
+import About    from './About';
+import Profile  from './Profile';
+import SignIn   from './SignIn';
+import SignOut  from './SignOut';
+
+export {
+    Home,
+    About,
+    Profile,
+    SignIn,
+    SignOut
+};
